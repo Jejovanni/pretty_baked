404d7c74d4733c636e20e4e76a566584e8001be7
jejovanni <jamaineekong@gmail.com>
2023-03-13 02:14:18 +0100

    removed github pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import Button from '../Button'
+import Button from './Button'
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 function Navbar() {
